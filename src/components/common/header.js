99e7cc73db3bf9ca1390d025a35f32adc6e45455
Pedro Rodrigues Lima <perliima72@gmail.com>
2016-06-20 21:51:04 -0300

    react-route was added

--- src/components/common/header.js
+++ src/components/common/header.js
@@ -1,18 +1,21 @@
 "use strict";
 
 var React = require("react");
+var Router = require("react-router");
+var Link = Router.Link;
 
 var Header = React.createClass({
 	render: function() {
 		return (
 			<nav className="navbar navbar-default">
 				<div className="conteiner-fluid">
-					<a href="/" className="navbar-brand">
-						<img className=" logo" src="images/logo.png"/>
-					</a>
+					<Link to="app" className="navbar-brand">
+						<img className=" logo" src="images/llogo.png"/>
+					</Link>
 					<ul className="nav navbar-nav">
-						<li><a href="/">Home</a></li>
-						<li><a href="/#about">About</a></li>
+						<li><Link to="app">Home</Link></li>
+						<li><Link to="authors">Authors</Link></li>
+						<li><Link to="about">About</Link></li>
 					</ul>
 				</div>
 			</nav>
